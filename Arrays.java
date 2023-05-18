65a8b4435b52dbf5765e24c9aff3b08602807cf7
Adrian Corrionero Castillo <acorrionerocastillo@gmail.com>
2023-05-18 20:35:43 +0200

    Generacion de primer y segundo metodo

--- Arrays.java
+++ Arrays.java
@@ -1,4 +1,42 @@
 package Arrays;
 public abstract class Arrays{
 	int [] Array;
+	/**
+	 * Metodo que calcula media aritmetica del array
+	 * @param array del que calcular la media
+	 * @return media aritmetica
+	 */
+	public static double media(int array[]) {
+		int suma = 0;
+		for (int i = 0; i < array.length; i++) {
+			suma = suma + array[i];
+		}
+		double media = (double) suma / array.length;
+		return media;
+	}
+	/**
+	 * metodo para calcular la mediana (si el numero es par utilizamos
+	 * los dos numeros centrales, si es impar, solamente el numero central)
+	 * @param array del que calcular el maximo
+	 * @return numero maximo
+	 */
+	public static double mediana(int[] array) {
+		Arrays.sort(array);
+		double mediana;
+		int mitad = array.length / 2;
+		if (array.length % 2 == 0) {
+			mediana = (double) (array[mitad - 1] + array[mitad]) / 2;
+		} else {
+			mediana = (double) array[mitad];
+		}
+		return mediana;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
